1a95a0ee40feb5f3b53d5b73bf9b458815baa860
Lorenzo Barbagli (ubuntu) <lorenzo@lorenzo-ub-12.04>
2015-02-23 21:45:17 +0100

    added some explanations to main activity

--- SnappingListViewApp/app/src/main/java/com/brl/snappinglistviewapp/MainActivity.java
+++ SnappingListViewApp/app/src/main/java/com/brl/snappinglistviewapp/MainActivity.java
@@ -7,7 +7,12 @@ import android.widget.ArrayAdapter;
 import java.util.ArrayList;
 import java.util.List;
 
-
+/**
+ * Simple Main Activity to show SnappingListView functionality.
+ *
+ * @author Lorenzo Barbagli
+ * @see <a href="http://www.lorenzobarbagli.net">www.lorenzobarbagli.net</a>
+ */
 public class MainActivity extends ActionBarActivity {
 
     @Override
@@ -15,17 +20,24 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Initialize the SnappingListView
         SnappingListView mSnappingListView = (SnappingListView) findViewById(R.id.snapping_list_view);
 
+        // Here we create a simple Adapter using a List<String>
         List<String> mListViewObjects = new ArrayList<>();
 
         for (int i = 0; i < 100; i++) {
             mListViewObjects.add("Element_" + i);
         }
 
-        mSnappingListView.setSnappingSpeed(1000);
-
         ArrayAdapter<String> mAdapter = new ArrayAdapter<>(this, R.layout.list_row, mListViewObjects);
+
+        // You can change snapping speed
+        mSnappingListView.setSnappingSpeed(700);
+
+        // Set the adapter
         mSnappingListView.setAdapter(mAdapter);
+
+        //That's all :)
     }
 }
